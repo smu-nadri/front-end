76a701fffd87f38a0e85fa571b918e50521d3876
hwakyung <hwakyung5531@gmail.com>
2022-08-30 16:51:33 +0900

    proj: 앨범 리스트 화면 수정 GridLayout(나드리앨범), GridView(마이앨범, 달력앨범) 사용 GridAdapter.java, album_view_item.xml 파일 생성 및 작성 album_view.xml 수정

--- app/src/main/java/com/example/nadri4_edit1/AlbumView.java
+++ app/src/main/java/com/example/nadri4_edit1/AlbumView.java
@@ -6,18 +6,26 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.example.nadri4_edit1.GridAdapter;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class AlbumView extends AppCompatActivity {
 
     ImageView folder1, folder2, folder3;
     ImageButton imgbtn_calendar;
     GridLayout testFolderLayout;
+    GridView gridView;
 
     String src;
 
@@ -31,9 +39,15 @@ public class AlbumView extends AppCompatActivity {
         folder2 = (ImageView) findViewById(R.id.imageView2);
         folder3 = (ImageView) findViewById(R.id.imageView3);
 
+        gridView = (GridView) findViewById(R.id.gvYearAlbum);
+
         imgbtn_calendar = (ImageButton) findViewById(R.id.calendar_button);
 
         testFolderLayout = (GridLayout) findViewById(R.id.testFolderLayout);
+
+        folder1.setClipToOutline(true);
+        folder2.setClipToOutline(true);
+        folder3.setClipToOutline(true);
 
         //이미지버튼(앨범버튼) 이벤트 -> 앨범뷰로 이동
         imgbtn_calendar.setOnClickListener(new View.OnClickListener() {
@@ -44,8 +58,9 @@ public class AlbumView extends AppCompatActivity {
             }
         });
 
-
-
+        GridAdapter gAdapter = new GridAdapter(this);
+        gAdapter.setItem(ReqServer.yearAlbumList);
+        gridView.setAdapter(gAdapter);
 
     }
 }
