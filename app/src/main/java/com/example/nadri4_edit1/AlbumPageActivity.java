0fb0857770ad16813f6f4e0fe308a7da4fc4fa4d
hwakyung <hwakyung5531@gmail.com>
2022-09-03 15:40:35 +0900

    proj: 주석 추가 및 삭제, 로그 태그 변경

--- app/src/main/java/com/example/nadri4_edit1/AlbumPageActivity.java
+++ app/src/main/java/com/example/nadri4_edit1/AlbumPageActivity.java
@@ -64,7 +64,6 @@ public class AlbumPageActivity extends AppCompatActivity {
         tvPageDate = (TextView) findViewById(R.id.tvPageDate);
         btnSave = (Button) findViewById(R.id.btnSave);
 
-        Log.d("GOO", "well come");
         //서버로 사진 정보 전송하기
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,14 +75,15 @@ public class AlbumPageActivity extends AppCompatActivity {
         //인텐트
         Intent getDateIntent = getIntent();
         int iDay = getDateIntent.getIntExtra("SelectedDATE",-1);
+
         //화면 설정
-        String title;
-        if (iDay == -1){
+        String title;   //제목 설정
+        if (iDay == -1){    //마이앨범이거나 월별앨범일 경우
             title = getDateIntent.getStringExtra("title");
             ReqServer.stitle = title;
             tvPageDate.setText(title);
         }
-        else {
+        else {  //캘린더에서 날짜를 선택한 경우
             setView(iDay);
             String tMonth, tDay;
             if(CalendarUtil.selectedDate.get(Calendar.MONTH) + 1 < 10) tMonth = "0" + (CalendarUtil.selectedDate.get(Calendar.MONTH) + 1);
@@ -96,24 +96,12 @@ public class AlbumPageActivity extends AppCompatActivity {
 
         //레이아웃 설정(열 = 2)
         RecyclerView.LayoutManager manager = new GridLayoutManager(AlbumPageActivity.recyclerView.getContext(), 2);
-        //recyclerView.LayoutManager(new GridLayoutManager(this, 2));
 
         //레이아웃 적용
         recyclerView.setLayoutManager(manager);
 
-
         //기존에 저장된 사진들 불러오기
         ReqServer.reqGetPages(AlbumPageActivity.this);
-
-
-        /*Intent getImageIntent = getIntent();
-        uriList = (ArrayList<Uri>)getImageIntent.getSerializableExtra("Photo");
-        //uriList.add((Uri) intent.getSerializableExtra("Photo"));
-        adapter = new MultiImageAdapter(uriList, getApplicationContext());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true));
-        */
-
 
         //이미지 가져오기 버튼
         btnGetImage.setOnClickListener(new View.OnClickListener() {
@@ -123,18 +111,8 @@ public class AlbumPageActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                intent.setAction(Intent.ACTION_OPEN_DOCUMENT);  //이거 수정했음!
+                intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
-
-                /*Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-                startActivityForResult(Intent.createChooser(intent,"다중 선택은 '포토'를 선택하세요."), 1);*/
-                //intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                //startActivityForResult(Intent.createChooser(intent, "다중선택"), 1);
-
-                /*startActivityForResult(Intent.createChooser(intent, "Add images"), SELECT_MULTIPLE_IMAGES);*/
             }
         });
     }
@@ -148,9 +126,6 @@ public class AlbumPageActivity extends AppCompatActivity {
         ReqServer.album.remove("thumbnail");
         ReqServer.album.remove("type");
     }
-
-    //기존에 저장된 앨범 레이아웃이 있다면 불러와서 보여줘야 함...
-    /*..{내용}..*/
 
     //앨범에서 액티비티로 돌아온 후 실행되는 메서드 = 앨범 레이아웃 생성!
     @RequiresApi(api = Build.VERSION_CODES.S)
@@ -173,7 +148,7 @@ public class AlbumPageActivity extends AppCompatActivity {
             }//이미지를 하나만 선택
             else{
                 ClipData clipData = data.getClipData();
-                Log.e("clipData", String.valueOf(clipData.getItemCount()));
+                Log.d("clipData", String.valueOf(clipData.getItemCount()));
 
                 for(int i=0; i< clipData.getItemCount(); i++){
                     Uri imageUri = clipData.getItemAt(i).getUri();  //선택한 이미지들의 uri를 가져온다.
@@ -186,34 +161,10 @@ public class AlbumPageActivity extends AppCompatActivity {
         }//이미지 선택함
     }
 
-    //파일경로 Uri를 String으로 바꿔주는 함수
-    private String getRealPathFromURI(Uri contentUri) {
-        if (contentUri.getPath().startsWith("/storage")) {
-            return contentUri.getPath();
-        }
-
-        String id = DocumentsContract.getDocumentId(contentUri).split(":")[1];
-        String[] columns = { MediaStore.Files.FileColumns.DATA };
-        String selection = MediaStore.Files.FileColumns._ID + " = " + id;
-        Cursor cursor = getContentResolver().query(MediaStore.Files.getContentUri("external"), columns, selection, null, null);
-        try {
-            int columnIndex = cursor.getColumnIndex(columns[0]);
-            if (cursor.moveToFirst()) {
-                return cursor.getString(columnIndex);
-            }
-        } finally {
-            cursor.close();
-        }
-        return null;
-    }
-
-
     @RequiresApi(api = Build.VERSION_CODES.O)
     private String dateFormat(Calendar calendar, int day){  //2022년 5월
 
         //년월 포맷
-        /*DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy   MM");
-        return date.format(formatter);*/
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH)+1;
 
@@ -222,20 +173,21 @@ public class AlbumPageActivity extends AppCompatActivity {
         return date;
     }
 
-    //화면 설정
+    //제목 설정
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void setView(int day){
         tvPageDate.setText(dateFormat(CalendarUtil.selectedDate, day));
     }
 
-    //수정해야됨
+    //어댑터 업데이트 해주기
     protected void setAdapterUpdated(){
+        //수정할 수 있을 듯
         adapter = new MultiImageAdapter(ReqServer.photoList, AlbumPageActivity.recyclerView.getContext());
 
-        //어댑터 적용\
         recyclerView.setAdapter(AlbumPageActivity.adapter);
     }
 
+    //선택한 사진의 태그 가져오기
     protected void setUriTags(Uri imageUri){
         JSONObject imageInfo = new JSONObject();    //사진 한 장의 uri와 태그들을 담을 변수
         ArrayList<JSONObject> tagsIndex = new ArrayList<>();    //사진 한 장의 태그들
@@ -255,6 +207,7 @@ public class AlbumPageActivity extends AppCompatActivity {
                         public void onSuccess(List<ImageLabel> labels) {
                             try {
                                 if (!labels.isEmpty()) {
+                                    //tagmap.json 파일 가져와서 변수에 저장
                                     InputStream inputS = getResources().openRawResource(R.raw.tagmap);
                                     byte[] buffer = new byte[inputS.available()];
                                     inputS.read(buffer);
@@ -263,9 +216,9 @@ public class AlbumPageActivity extends AppCompatActivity {
                                     JSONArray tagMap = new JSONArray(json);
 
                                     float confidence = labels.get(0).getConfidence();
-                                    for (ImageLabel label : labels) {   //차이가 15% 이하인 태그 5개까지만 넣기
+                                    for (ImageLabel label : labels) {   //차이가 15% 이하인 태그 3개까지만 넣기
                                         Log.d("HWA", "label: " + label.getIndex() + " " + label.getText());
-                                        if (tagsIndex.size() > 5) break;
+                                        if (tagsIndex.size() > 3) break;
                                         else if (confidence - label.getConfidence() > 0.15f)
                                             break;
                                         else tagsIndex.add(tagMap.getJSONObject(label.getIndex()));
@@ -273,7 +226,7 @@ public class AlbumPageActivity extends AppCompatActivity {
                                     imageInfo.put("tags", tagsIndex);   //태그들 넣기
                                 }
                                 ReqServer.photoList.add(imageInfo); //어뎁터에 들어갈 리스트에 사진 정보 넣기
-                                setAdapterUpdated(); //어뎁터 설정
+                                setAdapterUpdated(); //어댑터 설정
                             } catch(Exception e) {
                                 ReqServer.photoList.add(imageInfo);
                                 setAdapterUpdated();
@@ -288,7 +241,7 @@ public class AlbumPageActivity extends AppCompatActivity {
                         }
                     });
         } catch (Exception e) {
-            Log.e("HWA", "setUriTags Exception : " + e);
+            Log.e("AlbumPageActivity", "setUriTags Exception : " + e);
         }
     }
 }
