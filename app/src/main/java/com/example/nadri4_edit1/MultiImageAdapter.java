ff875a9a1e9f9d37a61064da4e24c479bed1d880
hwakyung <hwakyung5531@gmail.com>
2022-09-03 15:45:25 +0900

    proj: 주석, 로그 수정, Glide를 사용해 썸네일 적용하기

--- app/src/main/java/com/example/nadri4_edit1/MultiImageAdapter.java
+++ app/src/main/java/com/example/nadri4_edit1/MultiImageAdapter.java
@@ -71,18 +71,10 @@ public class MultiImageAdapter extends RecyclerView.Adapter<MultiImageAdapter.Vi
             InputStream inputStream = mContext.getContentResolver().openInputStream(imageUri);
             ExifInterface exif = new ExifInterface(inputStream);
 
-            //기본 썸네일이 있으면 가져오고 없으면 만들기
-            if(exif.hasThumbnail()){
-                //bitmap변환 라이브러리인 Glide사용
-                Glide.with(mContext).load(exif.getThumbnailBytes()).into(holder.image);
-            }
-            else {
-                //썸네일 만드는 작업 추가해야됨!!!
-                Glide.with(mContext).load(imageUri).into(holder.image);
-            }
+            Glide.with(mContext).load(imageUri).thumbnail(0.1f).into(holder.image);
 
         } catch (Exception e) {
-            Log.e("HWA", "onBindViewHolder Get Uri Thumbnail Error: " + e);
+            Log.e("MultiImageAdapter", "onBindViewHolder Get Uri Thumbnail Error: " + e);
         }
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -95,11 +87,11 @@ public class MultiImageAdapter extends RecyclerView.Adapter<MultiImageAdapter.Vi
         //코멘트 셋팅하기
         JSONObject imageObject = mData.get(holder.getBindingAdapterPosition());
         try {
-            if(imageObject.has("comment")) {
+            if(imageObject.has("comment")) {    //코멘트가 있으면
                 holder.comment.setText(imageObject.getString("comment"));
             }
             else {
-                if(imageObject.has("tags")){
+                if(imageObject.has("tags")){    //코멘트가 없고 태그가 있으면
                     JSONArray tags = new JSONArray(imageObject.getString("tags"));
                     String comment = "";
                     for(int i = 0; i < tags.length(); i++){
@@ -109,7 +101,7 @@ public class MultiImageAdapter extends RecyclerView.Adapter<MultiImageAdapter.Vi
                 }
             }
         } catch (JSONException e) {
-            Log.e("MultiImageAdapter", "get set comment error: " + e);
+            Log.e("MultiImageAdapter", "onBindViewHolder Get Set Comment Error: " + e);
         }
 
         holder.comment.addTextChangedListener(new TextWatcher() {
@@ -126,9 +118,10 @@ public class MultiImageAdapter extends RecyclerView.Adapter<MultiImageAdapter.Vi
             @Override
             public void afterTextChanged(Editable editable) {
                 try {
+                    //작성한 코멘트 서버로 보낼 변수에 넣기
                     mData.get(holder.getBindingAdapterPosition()).put("comment", editable.toString());
                 } catch (Exception e) {
-                    Log.e("MultiImageAdapter", "put comment error: " + e);
+                    Log.e("MultiImageAdapter", "Put Comment Error: " + e);
                 }
                 ;
             }
