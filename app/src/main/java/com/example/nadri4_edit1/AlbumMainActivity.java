a10c1e07fb0fc338211f2772c84f109de6c4aa0e
hwakyung <hwakyung5531@gmail.com>
2022-09-03 15:09:37 +0900

    proj: 앨범 매인 화면 수정 GridLayout id 변경, xml 변수 추가, visibility 설정 검색뷰로 이동할 때 액티비티명 변경에 따른 수정

--- app/src/main/java/com/example/nadri4_edit1/AlbumMainActivity.java
+++ app/src/main/java/com/example/nadri4_edit1/AlbumMainActivity.java
@@ -7,6 +7,7 @@ import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +16,9 @@ public class AlbumMainActivity extends AppCompatActivity {
 
     ImageView folder1, folder2, folder3;
     ImageButton imgbtn_calendar, imgbtn_search_a;
-    GridLayout testFolderLayout;
-    GridView gridView;
+    GridLayout glNadriAlbum;
+    GridView  gvCustomAlbum, gvYearAlbum;
+    LinearLayout nadriAlbum, customAlbum, dateAlbum;
 
     String src;
 
@@ -29,17 +31,34 @@ public class AlbumMainActivity extends AppCompatActivity {
         folder1 = (ImageView) findViewById(R.id.imageView1);
         folder2 = (ImageView) findViewById(R.id.imageView2);
         folder3 = (ImageView) findViewById(R.id.imageView3);
+        gvYearAlbum = (GridView) findViewById(R.id.gvYearAlbum);
+        imgbtn_calendar = (ImageButton) findViewById(R.id.calendar_button); //캘린더로 이동하는 버튼
+        imgbtn_search_a = (ImageButton) findViewById(R.id.a_search_button); //검색으로 이동하는 버튼
+        glNadriAlbum = (GridLayout) findViewById(R.id.glNadriAlbum);
+        nadriAlbum = (LinearLayout) findViewById(R.id.nadriAlbum);
+        customAlbum = (LinearLayout) findViewById(R.id.customAlbum);
+        dateAlbum = (LinearLayout) findViewById(R.id.dateAlbum);
 
-        gridView = (GridView) findViewById(R.id.gvYearAlbum);
-
-        imgbtn_calendar = (ImageButton) findViewById(R.id.calendar_button);
-        imgbtn_search_a = (ImageButton) findViewById(R.id.a_search_button);
-
-        testFolderLayout = (GridLayout) findViewById(R.id.testFolderLayout);
-
+        //사진 라운드 처리
         folder1.setClipToOutline(true);
         folder2.setClipToOutline(true);
         folder3.setClipToOutline(true);
+
+        //앨범리스트가 없으면 GONE으로 변경
+        if(ReqServer.customAlbumList.isEmpty()) {
+            customAlbum.setVisibility(View.GONE);
+            if(ReqServer.dateAlbumList.isEmpty()){
+                nadriAlbum.setVisibility(View.GONE);
+                dateAlbum.setVisibility(View.GONE);
+            }
+            else {
+                nadriAlbum.setVisibility(View.VISIBLE);
+                dateAlbum.setVisibility(View.VISIBLE);
+            }
+        }
+        else {
+            customAlbum.setVisibility(View.VISIBLE);
+        }
 
         //이미지버튼(앨범버튼) 이벤트 -> 앨범뷰로 이동
         imgbtn_calendar.setOnClickListener(new View.OnClickListener() {
@@ -54,14 +73,15 @@ public class AlbumMainActivity extends AppCompatActivity {
         imgbtn_search_a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent viewIntent = new Intent(getApplicationContext(), SearchActivity.class);
+                Intent viewIntent = new Intent(getApplicationContext(), SearchMainActivity.class);
                 startActivity(viewIntent);
             }
         });
 
-        GridAdapter gAdapter = new GridAdapter(this);
+        //처음 화면 셋팅
+        DateAlbumAdapter gAdapter = new DateAlbumAdapter(this);
         gAdapter.setItem(ReqServer.yearAlbumList);
-        gridView.setAdapter(gAdapter);
+        gvYearAlbum.setAdapter(gAdapter);
 
     }
 }
